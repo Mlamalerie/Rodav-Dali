8c75bc9b50d8590a0db0e75630793e92b6420198
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2021-03-10 00:14:44 +0100

    page albums js carré des carré + index scrool

--- js/boutique.js
+++ js/boutique.js
@@ -1,17 +1,35 @@
 function showQte(input) {
     let liste = document.getElementsByClassName("quantiteBay");
+    let contents = document.getElementsByClassName("content");
     
     if(input.checked) {
         console.log("caché");
         for(let i = 0; i < liste.length; i++) {
             console.log(liste[i]);
             liste[i].style.display = "none";
+          //  contents[i].classList.add("bottombottom1");
         }
     } else {
         console.log("visible");
         for(let i = 0; i < liste.length; i++) {
             console.log(liste[i]);
             liste[i].style.display = "inline";
+           //   contents[i].classList.remove("bottombottom1");
         }
+    }
+}
+
+function plus(id,max) {
+    let input = document.getElementById("nbQteCommande"+id);
+    if(input.value < max) {
+        let x = parseInt(input.value);
+        input.value = x+1;
+    }
+}
+function moin(id) {
+    let input = document.getElementById("nbQteCommande"+id);
+    if(input.value > 0) {
+         let x = parseInt(input.value);
+        input.value = x-1;
     }
 }
